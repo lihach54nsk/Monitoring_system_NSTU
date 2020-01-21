7d3f273033743d0becd32090e838adefd998e1a4
lihach54nsk <261-96@mail.ru>
2020-01-21 16:05:56 +0700

    added parameters of chart

--- src/Chart.js
+++ src/Chart.js
@@ -25,12 +25,29 @@ class Chart extends Component{
         }
     }
 
+    static defaultProps = {
+        displayTitle:true,
+        fontSize: 40,
+        displayLegend:true,
+        legendPosition:'right'
+    };
+
     render() {
         return (
             <div className="ChartName">
                 <Bar
                     data={this.state.chartData}
-                    options={{}}
+                    options={{
+                        title:{
+                            display:this.props.displayTitle,
+                            text:'The first my chart in React js',
+                            fontSize: this.props.fontSize
+                        },
+                        legend:{
+                            display: this.props.displayLegend,
+                            position: this.props.legendPosition
+                        }
+                    }}
                 />
             </div>
         )
