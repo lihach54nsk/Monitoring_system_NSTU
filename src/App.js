4bc87dcf7b3545a2ea0daee80391a8e8dad3cc8d
lihach54nsk <261-96@mail.ru>
2020-01-21 16:16:26 +0700

    first working chart

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chart from './Chart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Now I'm trying to fix the bug with this.
-        </p>
-        <a
-            className="App-link"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-        Show charts
-        </a>
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            chartData:{}
+        }
+    }
 
-      </header>
-      <div>
-        <Chart />
-      </div>
-    </div>
-  );
+    componentWillMount() {
+        this.getChartData();
+    }
+
+    getChartData(){ // get data for chart
+        this.setState({
+            chartData:{
+                labels:['1','2','3','4','5','6'],
+                datasets:[
+                    {
+                        label:'Idiotism',
+                        data:[1,2,3,4,5,6],
+                        backgroundColor:[
+                            'rgba(255,99,132,0.6)',
+                            'rgba(225,77,62,0.6)',
+                            'rgba(255,99,132,0.6)',
+                            'rgba(105,19,2,0.6)',
+                            'rgba(1,1,1,0.6)',
+                            'rgba(255,109,132,0.6)'
+                        ]
+                    }
+                ]
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload. Now I'm trying to fix the bug with this.
+                    </p>
+                    <a
+                        className="App-link"
+                        href=""
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Show charts
+                    </a>
+
+                </header>
+                <div>
+                    <Chart chartData={this.state.chartData}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Chart.js
+++ src/Chart.js
@@ -4,24 +4,8 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
 class Chart extends Component{
     constructor(props) {
         super(props);
-        this.state={
-            chartData:{
-                labels:['1','2','3','4','5','6'],
-                datasets:[
-                    {
-                        label:'Idiotism',
-                        data:[1,2,3,4,5,6],
-                        backgroundColor:[
-                            'rgba(255,99,132,0.6)',
-                            'rgba(225,77,62,0.6)',
-                            'rgba(255,99,132,0.6)',
-                            'rgba(105,19,2,0.6)',
-                            'rgba(1,1,1,0.6)',
-                            'rgba(255,109,132,0.6)'
-                        ]
-                    }
-                ]
-            }
+        this.state = {
+            chartData:props.chartData
         }
     }
 
