c6a8b1b84cc84acd13adbad45b1b87c627cfa7a5
lihach54nsk <261-96@mail.ru>
2020-04-20 12:54:00 +0700

    static list of devices

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -18,7 +18,7 @@ import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems, secondaryListItems, listItemsData } from './listItems';
 import Orders from './Orders';
 import LineChart from "./LineChart";
 import MoveChart from "./MoveChart";
@@ -148,6 +148,8 @@ export default function Dashboard() {
                         <ChevronLeftIcon/>
                     </IconButton>
                 </div>
+                <Divider/>
+                <List>{listItemsData}</List>
             </Drawer>
             <main className={classes.content}>
                 <div className={classes.appBarSpacer}/>
--- src/listItems.js
+++ src/listItems.js
@@ -9,6 +9,25 @@ import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import {Switch} from "@material-ui/core";
+
+function getDevices(i) {
+    const data = [1, 1, 2, 3];
+    for (;i < data.length; i++) {
+        switch (data[i]) {
+            case 0:
+                return <ListItem button><ListItemText primary={"Unknown" + " id"}/></ListItem>;
+            case 1:
+                return <ListItem button><ListItemText primary={"Temperature" + " id"}/></ListItem>;
+            case 2:
+                return <ListItem button><ListItemText primary={"Move" + " id"}/></ListItem>;
+            case 3:
+                return <ListItem button><ListItemText primary={"Impuls" + " id"}/></ListItem>;
+            default:
+                break;
+        }
+    }
+}
 
 export const mainListItems = (
     <div>
@@ -66,5 +85,14 @@ export const secondaryListItems = (
             </ListItemIcon>
             <ListItemText primary="Year-end sale" />
         </ListItem>
+    </div>
+);
+
+export const listItemsData = (
+    <div>
+        { getDevices(0) }
+        { getDevices(1) }
+        { getDevices(2) }
+        { getDevices(3) }
     </div>
 );
